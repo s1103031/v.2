37769d9ae2f8399c20ed99f552bcb87ac10267cb
江翊嘉 <s1103031@gm.pu.edu.tw>
2023-05-31 11:45:55 +0800

    v.2

--- app/src/main/java/com/example/jiang/MainActivity.kt
+++ app/src/main/java/com/example/jiang/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jiang
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         // 設定橫式螢幕
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         setContentView(R.layout.activity_main)
+
     }
+
 }
